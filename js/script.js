5d82ccc0b92f3cac51a77bfe045cf715aa0f15b0
Akromat <example@gmail.com>
2021-01-28 13:48:57 +0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,24 @@
+"use strict";
+
+// let number = 5;
+// const leftBorderWidth = 1;
+
+// number = 10;
+
+// console.log(4/0);
+// console.log('chair' * 5);
+
+// const surname = "Marziev";
+// const bool = true; 
+// let und;
+
+// console.log(under);
+
+// const obj = {
+//     name: "Akromat",
+//     age: 28,
+//     isMarried: false
+// };
+
+// console.log(obj.age);
+
