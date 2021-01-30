508ac5dcaa5f8885ee1f2828d217c7c0509ebdb5
Akromat <examle@gmail.com>
2021-01-30 16:24:38 +0300

    lesson 12

--- js/script.js
+++ js/script.js
@@ -22,3 +22,23 @@
 
 // console.log(obj.age);
 
+const numberOfFilms = prompt('Сколько фильмов вы уже посмотрели');
+
+const personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false
+};
+
+const a = prompt('Один из последнихпросмотренных фильмов'),
+      b = prompt('На сколько оцените'),
+      c = prompt('Один из последнихпросмотренных фильмов'),
+      d = prompt('На сколько оцените');
+
+
+personalMovieDB.movies[a] = b;      
+personalMovieDB.movies[c] = d;      
+
+console.log(personalMovieDB);
