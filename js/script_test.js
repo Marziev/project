1bf1d05b672b2a924a02bf9f3c57b3653e8c4cad
Akromat <examle@gmail.com>
2021-01-31 12:28:04 +0300

    lesson 14 true

--- js/script_test.js
+++ js/script_test.js
@@ -1,6 +1,8 @@
 "use strict"
 
+// УСЛОВИЯ=============================================================
 
+// const num = 50;
 
 // if (num > 100){
 //     console.log('Error');
@@ -12,19 +14,35 @@
 
 // (num === 50) ? console.log('OK') : console.log('Error');
 
-const num = 50;
 
-switch (num) {
-    case 49:
-        console.log('Не верно');
-        break;
-    case 60:
-        console.log('Не верно');
-        break;
-    case 50:
-        console.log('Верно');
-        break;
-    default:
-        console.log('В другой раз');
-        break;  
+// switch (num) {
+//     case 49:
+//         console.log('Не верно');
+//         break;
+//     case 60:
+//         console.log('Не верно');
+//         break;
+//     case 50:
+//         console.log('Верно');
+//         break;
+//     default:
+//         console.log('В другой раз');
+//         break;  
+// }
+
+// ЦИКЛЫ====================================================================================
+
+let num = 50;
+
+// while (num < 60) {
+//     console.log(num);
+//     num++;
+// }
+
+for (let i = 1; i < 10; i++){
+    if (i === 6){
+        // break;
+        continue;
+    }
+    console.log(i);
 }
