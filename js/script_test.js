ba48c706e4e96fc4c7f786323d5393f0db6f1d57
Akromat <examle@gmail.com>
2021-02-01 12:54:32 +0300

    lesson 16

--- js/script_test.js
+++ js/script_test.js
@@ -32,17 +32,42 @@
 
 // ЦИКЛЫ====================================================================================
 
-let num = 50;
+// let num = 50;
 
 // while (num < 60) {
 //     console.log(num);
 //     num++;
 // }
 
-for (let i = 1; i < 10; i++){
-    if (i === 6){
-        // break;
-        continue;
-    }
-    console.log(i);
+// for (let i = 1; i < 10; i++){
+//     if (i === 6){
+//         // break;
+//         continue;
+//     }
+//     console.log(i);
+// }
+
+// ФУНКЦИИ===========================================================
+
+let num = 10;
+
+function showFirstMessge(text) {
+    console.log(text);
+    num = 20; // можем переопределить глобальную переменную но только внутри цикла 
 }
+
+showFirstMessge("Hello world");
+
+function calc(a, b) {
+    return(a + b);
+}
+
+console.log(calc(10, 5));
+
+function ret() {
+    let num = 50;
+    return num;
+}
+
+const anotherNum = ret();
+console.log(anotherNum);
