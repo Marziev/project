529a1614aa675ae6d297c2e24a54b45869efb8b9
Akromat <example@gmail.com>
2021-02-16 22:13:48 +0300

    lesson 28 full

--- js/script_test.js
+++ js/script_test.js
@@ -323,18 +323,65 @@
 // // console.log(jonh.armor);
 // jonh.sayHello();
 
+// ДИНАМИЧЕСКАЯ ТИПИЗАЦИЯ ====================================================================
+
+// String
+// 1) 
+//  console.log(typeof(String(null)));
+//  console.log(typeof(String(4)));
+ 
+// 2) Конкатенация со строкой 
+//  console.log(typeof(4 + ''));
+
+//  const num = 5;
+
+//  console.log("https://vk.com/catalog/" + num);
+
+//  const fontSize = 26 + 'px';
+
+//  Number 
+// 1)
+// console.log(typeof(Number('4')));
+// 2) 
+// console.log(typeof(+'5'));
+// 3)
+// console.log(typeof(parseInt("15px", 10)));
+
+// To Boolean 
+
+// 0, null, undefined, '', NaN
+// 1)
+// let switcher = null;
+
+// if (switcher) {
+//     console.log('Working...');
+// }
+
+// switcher = 1;
+
+// if (switcher) {
+//     console.log('Working...');
+// }
+
+// 2)
+// console.log(typeof(Boolean('4')));
+
+// 3)
+// console.log(typeof(!!('466')));
+
+
 // ЗАДАЧИ НА СОБЕСЕДОВАНИЯХ ===============================================================
 
 // let x = 5;
 
 // alert(++x);
 
-// // let y = 1;
+// let y = 1;
 
-// // let x = y = 2;
+// let x = y = 2;
 
-// // alert(x);
-// // [ ] + false - null + true 
+// alert(x);
+// [ ] + false - null + true 
 
 // console.log(typeof([] + false));
 
