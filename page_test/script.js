b408135c9f960cc09c81af8d7b579873403c9846
Akromat <example@gmail.com>
2021-03-01 20:13:25 +0300

    lesson 31 full

--- page_test/script.js
+++ page_test/script.js
@@ -1,6 +1,7 @@
 'use strict';
 
-const btn = document.querySelector('button');
+const btns = document.querySelectorAll('button'),
+      overlay = document.querySelector('.overlay');
 
 // btn.onclick = function () {
 //     alert('work');
@@ -8,15 +9,26 @@ const btn = document.querySelector('button');
 
 let i = 0;
 const deleteElement = (e) => {
-    console.log(e.target);
-    i++;
-    if(i == 1) {
-        btn.removeEventListener('click', deleteElement);
-    }
+    console.log(e.currentTarget);
+    console.log(e.type);
+    // i++;
+    // if(i == 1) {
+    //     btn.removeEventListener('click', deleteElement);
+    // }
 };
 
-btn.addEventListener('click', deleteElement);
 
+
+// btn.addEventListener('click', deleteElement);
+// overlay.addEventListener('click', deleteElement);
+btns.forEach(btn => {
+    btn.addEventListener('click', deleteElement);
+});
+const link = document.querySelector('a');
+link.addEventListener('click', function(eve) {
+    eve.preventDefault();
+    console.log(eve.target);
+});
 
 // btn.addEventListener('mouseenter', (e) => {
 //     console.log(e.target);
